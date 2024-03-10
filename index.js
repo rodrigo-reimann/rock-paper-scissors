c2af8a2feaf6304f850de13292c5ccf63ecfa421
Rodrigo Baston <rodrigo.rbaston@gmail.com>
2024-03-09 20:25:01 -0800

    completed

---
+++ index.js
@@ -0,0 +1,47 @@
+const startButton = document.getElementById('startButton');
+startButton.addEventListener('click', () => {
+    playGame();
+})
+
+
+function getComputerChoice () {
+    const strings = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * strings.length);
+
+    return strings[randomIndex];
+};
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "scissors" ||
+        playerSelection === "scissors" && computerSelection === "paper" ||
+        playerSelection === "paper" && computerSelection === "rock") {
+            return "You win!";
+    } else if (playerSelection === computerSelection) {
+        return "You lose!";
+    } else {
+        return "It's a tie!";
+    }
+};
+
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt(`Round ${i + 1}: Enter your choice (rock, paper, or scissors):`).toLowerCase();
+        const computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
+
+        alert(`Round ${i + 1} Result: ${result}`)
+
+        if (result.includes("win")) {
+            playerScore++;
+        } else if (result.includes("lose")) {
+            computerScore++;
+        }
+    }
+
+    document.getElementById('results').textContent = `Player score: ${playerScore}, Computer score: ${computerScore}`
+}
+
+console.log(playGame());
